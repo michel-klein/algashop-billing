83eb45356051068bd503fd305e1dabfd1bd65f78
Michel Klein <michel.kps@outlook.com>
2026-06-10 18:49:43 -0300

    feat: billing scheduler

--- src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Invoice.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Invoice.java
@@ -108,6 +108,7 @@ public class Invoice extends AbstractAuditableAggregateRoot<Invoice> {
         }
         setPaidAt(OffsetDateTime.now());
         setStatus(InvoiceStatus.PAID);
+        setExpiresAt(null);
         registerEvent(new InvoicePaidEvent(this.getId(), this.getCustomerId(), this.getOrderId(), this.getPaidAt()));
     }
 
@@ -118,6 +119,7 @@ public class Invoice extends AbstractAuditableAggregateRoot<Invoice> {
         setCancelReason(cancelReason);
         setCanceledAt(OffsetDateTime.now());
         setStatus(InvoiceStatus.CANCELED);
+        setExpiresAt(null);
         registerEvent(new InvoiceCanceledEvent(this.getId(), this.getCustomerId(), this.getOrderId(), this.getCanceledAt()));
     }
 
