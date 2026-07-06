e48e3cd718f16fae8be2b375b1ff663a74dfd350
Michel Klein <michel.kps@outlook.com>
2026-07-06 12:39:01 -0300

    feat: mongodb transactions

--- src/test/java/com/algaworks/algashop/billing/application/invoice/AbstractApplicationIT.java
+++ src/test/java/com/algaworks/algashop/billing/application/invoice/AbstractApplicationIT.java
@@ -1,7 +1,7 @@
 package com.algaworks.algashop.billing.application.invoice;
 
 import com.algaworks.algashop.billing.utils.TestcontainerPostgreSQLConfig;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
